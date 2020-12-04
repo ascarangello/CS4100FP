f555d4c1d1cccd46f41f0e1a4ffb5be091079d7e
ascarangello <33673583+ascarangello@users.noreply.github.com>
2020-12-04 16:57:52 -0500

    added all movement types, added assasin, bowman, and champion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+import random
 import sys
 import numpy as np
 
@@ -6,16 +7,28 @@ import numpy as np
 NUM_COLS = 6
 WHITEDUKE = 1
 WHITEFOOTMAN = 2
+WHITEASSASSIN = 3
+WHITEBOWMAN = 4
+WHITECHAMPION = 5
 EMPTY = 0
 BLACKDUKE = -1
 BLACKFOOTMAN = -2
+BLACKASSASSIN = -3
+BLACKBOWMAN = -4
+BLACKCHAMPION = -5
 
 printable = {
-    EMPTY: "[-]",
+    EMPTY: "[--]",
     WHITEDUKE: "[WD]",
-    WHITEFOOTMAN: "[WF]",
     BLACKDUKE: "[BD]",
-    BLACKFOOTMAN: "[BF]"
+    WHITEFOOTMAN: "[WF]",
+    WHITEASSASSIN: "[WA]",
+    BLACKFOOTMAN: "[BF]",
+    BLACKASSASSIN: "[BA]",
+    WHITEBOWMAN: "[WB]",
+    BLACKBOWMAN: "[BB]",
+    WHITECHAMPION: "[WC]",
+    BLACKCHAMPION: "[BC]"
 }
 
 WHITE_TO_PLAY = True
@@ -26,12 +39,23 @@ SQUAREPLACEMENT = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 MOVE = 0
 JUMP = 1
 SLIDE = 2
+JUMPSLIDE = 3
+STRIKE = 4
+
 DUKEUP = [(-1, 0, SLIDE), (1, 0, SLIDE)]
 DUKEDOWN = [(0, 1, SLIDE), (0, -1, SLIDE)]
 
 FOOTMANUP = [(1, 0, MOVE), (-1, 0, MOVE), (0, 1, MOVE), (0, -1, MOVE)]
 FOOTMANDOWN = [(1, 1, MOVE), (1, -1, MOVE), (-1, -1, MOVE), (-1, 1, MOVE), (0, -2, MOVE)]
 
+ASSASSINUP = [(0, -1, JUMPSLIDE), (-1, 1, JUMPSLIDE), (1, 1, JUMPSLIDE)]
+ASSASSINDOWN = [(0, 1, JUMPSLIDE), (-1, -1, JUMPSLIDE), (1, -1, JUMPSLIDE)]
+
+BOWMANUP = [(0, -1, MOVE), (1, 0, MOVE), (-1, 0, MOVE), (-2, 0, JUMP), (2, 0, JUMP), (0, 2, JUMP)]
+BOWMANDOWN = [(0, -1, MOVE), (1, 1, MOVE), (-1, 1, MOVE), (0, -2, STRIKE), (1, -1, STRIKE), (-1, -1, STRIKE)]
+
+CHAMPIONUP = [(1, 0, MOVE), (-1, 0, MOVE), (0, 1, MOVE), (0, -1, MOVE), (2, 0, JUMP), (-2, 0, JUMP), (0, 2, JUMP), (0, -2, JUMP)]
+CHAMPIONDOWN = [(1, 0, STRIKE), (-1, 0, STRIKE), (0, 1, STRIKE), (0, -1, STRIKE), (2, 0, JUMP), (-2, 0, JUMP), (0, 2, JUMP), (0, -2, JUMP)]
 
 class Tile:
     def __init__(self, type, upMoves, downMoves):
@@ -46,20 +70,30 @@ EMPTYTILE = Tile(EMPTY, 0, 0)
 WHITEDUKETILE = Tile(WHITEDUKE, DUKEUP, DUKEDOWN)
 WHITEFOOTMANTILE = Tile(WHITEFOOTMAN, FOOTMANUP, FOOTMANDOWN)
 BLACKFOOTMANTILE = Tile(BLACKFOOTMAN, FOOTMANUP, FOOTMANDOWN)
-
 BLACKDUKETILE = Tile(BLACKDUKE, DUKEUP, DUKEDOWN)
+
+WHITEASSASSINTILE = Tile(WHITEASSASSIN, ASSASSINUP, ASSASSINDOWN)
+BLACKASSASSINTILE = Tile(BLACKASSASSIN, ASSASSINUP, ASSASSINDOWN)
+
+WHITEBOWMANTILE = Tile(WHITEBOWMAN, BOWMANUP, BOWMANDOWN)
+BLACKBOWMANTILE = Tile(BLACKBOWMAN, BOWMANUP, BOWMANDOWN)
+
+WHITECHAMPIONTILE = Tile(WHITECHAMPION, CHAMPIONUP, CHAMPIONDOWN)
+BLACKCHAMPIONTILE = Tile(BLACKCHAMPION, CHAMPIONUP, CHAMPIONDOWN)
+
 
 
 def gen_legal_moves(board, row, col):
     tile = board.board[row][col]
     legal_moves = []
+    moveTypes = []
     validDirs = []
     if tile.isUp:
         validDirs = tile.upMoves
     else:
         validDirs = tile.downMoves
     for x_delta, y_delta, moveType in validDirs:
-        if tile.type < 0:
+        if (tile.type < 0 and row != 6) or (tile.type > 0 and row == 0):
             y_delta *= -1
         if (col + x_delta < 0) or (col + x_delta >= NUM_COLS):
             continue
@@ -88,11 +122,22 @@ def gen_legal_moves(board, row, col):
                 if targetType == EMPTY or (targetType < EMPTY and tile.type > EMPTY) or (
                         targetType > EMPTY and tile.type < EMPTY):
                     legal_moves.append((row + y_delta, col + x_delta))
+                    moveTypes.append(moveType)
+
         if moveType == JUMP:
             targetType = board.board[row + y_delta][col + x_delta].type
             if targetType == EMPTY or (targetType < EMPTY and tile.type > EMPTY) or (
                     targetType > EMPTY and tile.type < EMPTY):
                 legal_moves.append((row + y_delta, col + x_delta))
+                moveTypes.append(moveType)
+
+        if moveType == STRIKE:
+            targetType = board.board[row + y_delta][col + x_delta].type
+            if (targetType < EMPTY and tile.type > EMPTY) or (
+                    targetType > EMPTY and tile.type < EMPTY):
+                legal_moves.append((row + y_delta, col + x_delta))
+                moveTypes.append(moveType)
+
         if moveType == SLIDE:
             x_index = x_delta
             y_index = y_delta
@@ -108,23 +153,52 @@ def gen_legal_moves(board, row, col):
                     continue
                 targetType = board.board[y_pos][x_pos].type
                 if targetType == EMPTY:
-                    legal_moves.append(((y_pos, x_pos)))
+                    legal_moves.append((y_pos, x_pos))
                     x_index += x_delta
                     y_index += y_delta
                 elif (targetType < EMPTY and tile.type > EMPTY) or (targetType > EMPTY and tile.type < EMPTY):
-                    legal_moves.append(((y_pos, x_pos)))
+                    legal_moves.append((y_pos, x_pos))
+                    moveTypes.append(moveType)
                     stop = True
                     continue
                 else:
                     stop = True
-    return legal_moves
+
+        if moveType == JUMPSLIDE:
+            x_index = x_delta
+            y_index = y_delta
+            stop = False
+            while not stop:
+                x_pos = col + x_index
+                y_pos = row + y_index
+                if (x_pos < 0) or (x_pos >= NUM_COLS):
+                    stop = True
+                    continue
+                if (y_pos < 0) or (y_pos >= NUM_COLS):
+                    stop = True
+                    continue
+                targetType = board.board[y_pos][x_pos].type
+                if targetType == EMPTY:
+                    legal_moves.append((y_pos, x_pos))
+                    moveTypes.append(moveType)
+                    x_index += x_delta
+                    y_index += y_delta
+                elif (targetType < EMPTY and tile.type > EMPTY) or (targetType > EMPTY and tile.type < EMPTY):
+                    legal_moves.append((y_pos, x_pos))
+                    moveTypes.append(moveType)
+                    x_index += x_delta
+                    y_index += y_delta
+                else:
+                    x_index += x_delta
+                    y_index += y_delta
+    return legal_moves, moveTypes
 
 
 class Board:
     def __init__(self, size):
         self.board = [[EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE],
                       [EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE],
-                      [EMPTYTILE, EMPTYTILE, BLACKFOOTMANTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE],
+                      [EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE],
                       [EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE],
                       [EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE],
                       [EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE, EMPTYTILE]]
@@ -151,11 +225,14 @@ def showPotentialMoves(board, moves):
 
 
 # Will move the unit to the move position based on the assumption it is a legal move and return the new board
-def moveUnit(board, move, row, col):
+def moveUnit(board, move, moveType, row, col):
     newState = copy.deepcopy(board)
     toMove = newState.board[row][col]
-    newState.board[move[0]][move[1]] = toMove
-    newState.board[row][col] = EMPTYTILE
+    if moveType != STRIKE:
+        newState.board[move[0]][move[1]] = toMove
+        newState.board[row][col] = EMPTYTILE
+    elif moveType == STRIKE:
+        newState.board[move[0]][move[1]] = EMPTYTILE
     toMove.isUp = not toMove.isUp
     return newState
 
@@ -185,9 +262,31 @@ def gen_legal_placements(board):
             legal_placements.append((target[0] + y_delta, target[1] + x_delta))
     return legal_placements
 
+class Bags:
+  def __init__(self):
+    self.playerBag = [WHITEFOOTMANTILE, WHITEASSASSINTILE, WHITEBOWMANTILE, WHITECHAMPIONTILE]
+    self.aiBag = [BLACKFOOTMANTILE, BLACKASSASSINTILE, BLACKBOWMANTILE, BLACKCHAMPIONTILE]
+
+  def pull(self, isPlayersTurn):
+    if isPlayersTurn:
+      if len(self.playerBag) == 0:
+        print("Bag is empty! Move a unit instead!")
+        return EMPTYTILE
+      toRemove = self.playerBag[random.randrange(len(self.playerBag))]
+      print("You drew a " + printable[toRemove.type])
+      self.playerBag.remove(toRemove)
+      return toRemove
+    else:
+      if len(self.aiBag) == 0:
+        return EMPTYTILE
+      toRemove = self.aiBag[random.randrange(len(self.aiBag))]
+      self.aiBag.remove(toRemove)
+      return toRemove
+
 
 def play():
     GAMEBOARD = Board(NUM_COLS)
+    UNITBAG = Bags()
     startingCol = int(input("Enter the column of your Duke (0 - 5): "))
     GAMEBOARD = placeUnit(GAMEBOARD, (5, startingCol), WHITEDUKETILE)
     GAMEBOARD.print_board()
@@ -207,13 +306,25 @@ def play():
         if action == 0:
             col = int(input("Enter the column of the piece to move: "))
             row = NUM_COLS - 1 - int(input("Enter the row of the piece to move: "))
-            moveOptions = gen_legal_moves(GAMEBOARD, row, col)
+            moveOptions, moveTypes = gen_legal_moves(GAMEBOARD, row, col)
+            if len(moveOptions) == 0:
+                print("No moves available for the selected unit, please try again")
+                continue
             showPotentialMoves(GAMEBOARD, moveOptions)
             moveIndex = int(input("Select where you would like to move this unit to(0 - " + str(len(moveOptions) - 1) + "): "))
-            GAMEBOARD = moveUnit(GAMEBOARD, moveOptions[moveIndex], row, col)
+            GAMEBOARD = moveUnit(GAMEBOARD, moveOptions[moveIndex], moveTypes[moveIndex], row, col)
             WHITE_TO_PLAY = False
         if action == 1:
             #print("TODO: Handle bag drawing logic")
+            newUnit = UNITBAG.pull(True)
+            if newUnit == EMPTYTILE:
+                continue
+            placementOptions = gen_legal_placements(GAMEBOARD)
+            showPotentialMoves(GAMEBOARD, placementOptions)
+            moveIndex = int(input("Select where you would like to place the new unit (0 - " + str(
+                len(placementOptions) - 1) + "): "))
+            GAMEBOARD = placeUnit(GAMEBOARD, placementOptions[moveIndex], newUnit)
+            GAMEBOARD.print_board()
             WHITE_TO_PLAY = False
 
 
